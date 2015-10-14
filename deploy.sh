404bc20cb3faf6e6ad1871c0af917bed55060c75
Mathew Pannell <mat2411pan@gmail.com>
2015-10-14 12:06:07 +0100

    Continuing! Now trying permissions

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo installing firebase tools
-npm install -g firebase-tools
+#echo installing firebase tools
+#npm install -g firebase-tools
 echo Deploying to firebase
 firebase deploy --non-interactive
